caf8aeda468cd938f5568dfd9cee64ffd737df4b
ardx3000 <daniel.ardx@gmail.com>
2024-07-12 17:51:59 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Sudoku
  
+ ## Description:
+
+ A simple sudoku game created in .NET usign RayLib for the GUI.
+ Beeside the actual sudoku game I want to implement different auto solving systems.
---
+++ Sudoku/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Sudoku
+{
+    /*
+     * Create a a class for the gameboard.
+     * Create a class for the game logic: User input, constrains, winning conditions.
+     * Create a class for different automatic solving alghorithms.
+     * Create the GUI : menu , game board, user interactions.
+     */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
